66b197f610c4f5b4ac94a1272ab0db117ab1ca26
Kelo <1376312818@qq.com>
2015-05-01 10:10:19 +0800

    update

--- autoScroll/autoScroll.user.js
+++ autoScroll/autoScroll.user.js
@@ -7,6 +7,9 @@
 // @icon            https://raw.githubusercontent.com/GH-Kelo/userscript/master/autoScroll/img/autoScroll.png
 // @include         *
 // @run-at          document-start
+// @homepageURL            https://github.com/GH-Kelo/userscript/tree/master/autoScroll
+// @updateURL            https://raw.githubusercontent.com/GH-Kelo/userscript/master/autoScroll/autoScroll.user.js
+// @downloadURL            https://raw.githubusercontent.com/GH-Kelo/userscript/master/autoScroll/autoScroll.user.js
 // @note            2015.3.3  beta 0.1.0 简单修改自http://bbs.kafan.cn/thread-1795464-1-1.html，加入循环（不影响网页加载），使用“document-start”，在网页加载完前即滚动，更加迅速。
 // @note            2015.3.4  beta 0.1.1 修复V1.0循环问题。
 // @note            2015.3.4  beta 0.1.5 更新至1.5大幅度优化代码、代码逻辑，增加显示错误功能，更快的速度。
@@ -104,7 +107,6 @@
         }
         height = height + adjustment;
         window.scrollTo(0, height);
-        console.log("运行了");
         //if (t == times[0]) clearInterval(timer);
       }, times[1]);
     }, delay);
